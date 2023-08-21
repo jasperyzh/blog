29610cd864a31eed0d555c7ff6f9886fe410cf31
jaspery <jasper.yzh@gmail.com>
2023-08-21 17:18:10 +0800

    setup ghactions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jasperyong.com",
+  base: '/',
   experimental: {
     assets: true,
     viewTransitions: true
